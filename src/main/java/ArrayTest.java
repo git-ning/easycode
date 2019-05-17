5089b82698360b729aa2e7455975df539dc099e6
王宁 <wangning040@ke.com>
2019-05-17 09:40:00 +0800

    Create new file ArrayTest

---
+++ src/main/java/ArrayTest.java
@@ -0,0 +1,33 @@
+/**
+ * 一些常用的数组操作练习
+ *
+ * @author wangning040@ke.com
+ * @date 2019-05-17 09:27
+ */
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        // 根据下标随机读取元素
+        int[] arr = new int[] {3, 1, 2, 5, 4, 9 , 7, 2};
+        System.out.println(arr[3]);
+
+        // 更新元素
+        arr[5] = 10;
+        System.out.println(arr[5]);
+
+        // 插入元素，不涉及扩容
+        int[] arr2;
+        int size;
+
+
+    }
+
+    // 插入新元素
+    public static void insert(int index, int data) {
+        // 判断是否越界
+
+    }
+
+
+
+}
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * 数组相关操作测试
  *
@@ -12,5 +14,37 @@ public class ArrayTest {
     public void outOfBandTest() {
         int[] a = new int[3];
         a[3] = 10;
+    }
+
+    @Test
+    public void ifElseTest() {
+        int x = 2;
+        if (x == 1 || x == 3) {
+            System.out.println("if statement");
+        }
+        if (x == 2 || x == 3) {
+            System.out.println("else if statement");
+        }
+    }
+
+    @Test
+    public void newArrayListTest() {
+        // 不指定ArrayList初始大小
+        Long startTime = System.currentTimeMillis();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            list1.add(i);
+        }
+        Long endTime = System.currentTimeMillis();
+        System.out.println("Without init size time cost(ms): " + (endTime - startTime));
+
+        // 指定ArrayList初始大小
+        Long startTime2 = System.currentTimeMillis();
+        ArrayList<Integer> list2 = new ArrayList<>(100000);
+        for (int i = 0; i < 100000; i++) {
+            list2.add(i);
+        }
+        Long endTime2 = System.currentTimeMillis();
+        System.out.println("With init size 100000 time cost(ms): " + (endTime2 - startTime2));
     }
 }
